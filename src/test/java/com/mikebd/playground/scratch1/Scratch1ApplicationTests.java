bd2c2fcc7bb1e153c0398a4748d2bf2a15fde759
Michael Ben-David <mikebd>
2020-10-15 19:43:20 -0400

    Initial commit

---
+++ src/test/java/com/mikebd/playground/scratch1/Scratch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikebd.playground.scratch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scratch1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
