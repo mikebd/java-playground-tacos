025e569973928d9dfa995a005790495d992b0f02
Michael Ben-David <mikebd>
2020-10-15 21:02:05 -0400

    Refactor to support unit test runner in addition to command line, shared test data between these, additional unit tests could be written.

--- src/main/java/com/mikebd/playground/scratch1/service/StockPriceBestProfitService.java
+++ src/main/java/com/mikebd/playground/scratch1/service/StockPriceBestProfitService.java
@@ -1,5 +1,7 @@
 package com.mikebd.playground.scratch1.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -12,33 +14,32 @@ import java.util.Arrays;
 @Slf4j
 public class StockPriceBestProfitService {
     public boolean run() {
+        log.info("Start StockPriceBestProfitService");
         boolean pass = true;
-        pass &= test(new long [] {13, 10, 8, 4, 10}, 6);
-        pass &= test(new long [] {6, 0, -1, 10}, 11);
-        pass &= test(new long [] {5, 10, 20, 30, 1, 0}, 25);
+        pass &= test(testData1);
+        pass &= test(testData2);
+        pass &= test(testData3);
         if (pass) {
             log.info("Overall: PASS");
         } else {
             log.info("Overall: FAIL");
         }
+        log.info("End StockPriceBestProfitService");
         return pass;
     }
 
     private static boolean test(final long[] input, final long expected) {
         final long actual = calculate(input);
         final boolean pass = expected == actual;
-        if (pass) {
-            log.info("PASS " + Arrays.toString(input) + " expect " + expected + " = " + actual);
-        } else {
-            log.error("FAIL " + Arrays.toString(input) + " expect " + expected + " = " + actual);
-        }
+        final String logMessage = (pass ? "PASS " : "FAIL ") + Arrays.toString(input) + " expect " + expected + " = " + actual;
+        if (pass) log.info(logMessage); else log.error(logMessage);
         return pass;
     }
 
+    // Oddly, test cases expect negative stock prices to be considered normal
     private static long calculate(long[] prices) {
         if (prices == null || prices.length == 0) return 0;
 
-        // Oddly, test cases expect negative stock prices to be considered normal
         final int maxIndex = prices.length - 1;
 
         // Init to conditions on the final day
@@ -55,4 +56,18 @@ public class StockPriceBestProfitService {
 
         return profit;
     }
+
+    @AllArgsConstructor @Getter
+    static class TestData {
+        private final long[] input;
+        private final long expected;
+    }
+
+    static boolean test(final TestData testData) {
+        return test(testData.getInput(), testData.getExpected());
+    }
+
+    static final TestData testData1 = new TestData(new long[] {13, 10, 8, 4, 10}, 6);
+    static final TestData testData2 = new TestData(new long[] {6, 0, -1, 10}, 11);
+    static final TestData testData3 = new TestData(new long[] {5, 10, 20, 30, 1, 0}, 25);
 }
