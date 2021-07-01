28c8dcf8bb7479bd98ba853302246338801b732f
Michael Ben-David <michael.ben-david@groupbyinc.com>
2021-06-30 22:17:38 -0400

    Taco Cloud home controller

--- src/main/java/com/mikebd/playground/scratch1/Scratch1Application.java
+++ src/main/java/com/mikebd/playground/scratch1/Scratch1Application.java
@@ -1,37 +1,16 @@
 package com.mikebd.playground.scratch1;
 
-import com.mikebd.playground.scratch1.service.FibonacciService;
-import com.mikebd.playground.scratch1.service.RomanNumberService;
-import com.mikebd.playground.scratch1.service.StockPriceBestProfitService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @AllArgsConstructor
 @SpringBootApplication
 @Slf4j
-public class Scratch1Application implements CommandLineRunner {
+public class Scratch1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Scratch1Application.class, args);
     }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("Start");
-        boolean pass = fibonacciService.run();
-        pass &= romanNumberService.run();
-        pass &= stockPriceBestProfitService.run();
-        log.info("End");
-        final int exitCode = pass ? 0 : 1;
-        System.exit(SpringApplication.exit(ctx, () -> exitCode));
-    }
-
-    private final ApplicationContext ctx;
-    private final FibonacciService fibonacciService;
-    private final RomanNumberService romanNumberService;
-    private final StockPriceBestProfitService stockPriceBestProfitService;
 }
---
+++ src/main/java/com/mikebd/playground/scratch1/tacos/HomeController.java
@@ -0,0 +1,13 @@
+package com.mikebd.playground.scratch1.tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
